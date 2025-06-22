90eb0e389240c847cdf7868b354da90cffe59082
Sajith Silva <sajiths@pagero.com>
2025-06-22 08:47:40 +0530

    Rename ExpenseTable.css to ExpenseImport.css and update import in ExpenseImport.jsx.

---
+++ src/components/ExpenseImport.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './ExpenseImport.css';
+import Papa from 'papaparse';
+
+const ExpenseImport = () => {
+  // ...existing code from ExpenseTable, unchanged...
+};
+
+export default ExpenseImport;
